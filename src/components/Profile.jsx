e067d92ae08adab72f13746e11744d91a8ead19f
AlexSerg01 <alex.box.work@icloud.com>
2024-04-23 12:06:53 +0200

    finish :)

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,28 +1,28 @@
 // src/Product.jsx
 import styles from "./Profile.module.css";
 
-const Product = ({ name, tag, location, image, stats }) => {
+const Profile = ({ name, tag, location, image, stats }) => {
   return (
-    <div className={styles.profile}>
-      <div>
-        <img className={styles.image} src={image} alt="User avatar" />
-        <p className={styles.name}>{name}</p>
-        <p className={styles.tag}>@{tag}</p>
-        <p className={styles.location}>{location}</p>
+    <div className={styles.ProfileContainer}>
+      <div className={styles.ProfileHead}>
+        <img className={styles.ProfileImage} src={image} alt="User avatar" />
+        <p className={styles.ProfileName}>{name}</p>
+        <p className={styles.ProfileTag}>@{tag}</p>
+        <p className={styles.ProfileLocation}>{location}</p>
       </div>
 
-      <ul className={styles.profileInformation}>
-        <li className={styles.profileInformationItem}>
+      <ul className={styles.ProfileInformation}>
+        <li className={styles.ProfileInformationItem}>
           <span>Followers</span>
           <span className={styles.integer}>{stats.followers}</span>
         </li>
 
-        <li className={styles.profileInformationItem}>
+        <li className={styles.ProfileInformationItem}>
           <span>Views</span>
           <span className={styles.integer}>{stats.views}</span>
         </li>
 
-        <li className={styles.profileInformationItem}>
+        <li className={styles.ProfileInformationItem}>
           <span>Likes</span>
           <span className={styles.integer}>{stats.likes}</span>
         </li>
@@ -31,4 +31,4 @@ const Product = ({ name, tag, location, image, stats }) => {
   );
 };
 
-export default Product;
+export default Profile;
